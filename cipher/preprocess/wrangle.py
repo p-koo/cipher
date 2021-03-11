7fb32f7428439f299543783b50f53431f96f41ca
Eduardo Esteva <eduardoesteva@Eduardos-MacBook-Pro.local>
2021-03-11 17:55:07 -0500

    fixed indentation errors

--- cipher/preprocess/wrangle.py
+++ cipher/preprocess/wrangle.py
@@ -10,17 +10,17 @@ def convert_one_hot(sequence, max_length=None):
 
 	one_hot_seq = []
 	for seq in sequence:
-      		seq = seq.upper()
-      		seq_length = len(seq)
-      		one_hot = np.zeros((4,seq_length))
-      		index = [j for j in range(seq_length) if seq[j] == 'A']
-      		one_hot[0,index] = 1
-      		index = [j for j in range(seq_length) if seq[j] == 'C']
-      		one_hot[1,index] = 1
-      		index = [j for j in range(seq_length) if seq[j] == 'G']
-      		one_hot[2,index] = 1
-      		index = [j for j in range(seq_length) if (seq[j] == 'U') | (seq[j] == 'T')]
-      		one_hot[3,index] = 1
+		seq = seq.upper()
+		seq_length = len(seq)
+		one_hot = np.zeros((4,seq_length))
+		index = [j for j in range(seq_length) if seq[j] == 'A']
+		one_hot[0,index] = 1
+		index = [j for j in range(seq_length) if seq[j] == 'C']
+		one_hot[1,index] = 1
+		index = [j for j in range(seq_length) if seq[j] == 'G']
+		one_hot[2,index] = 1
+		index = [j for j in range(seq_length) if (seq[j] == 'U') | (seq[j] == 'T')]
+		one_hot[3,index] = 1
 
 		# handle boundary conditions with zero-padding
 		if max_length:
@@ -156,32 +156,32 @@ def bedtools_intersect(dnase_bed_path,tf_bed_path,output_bed_path):
 
 # TODO: random split into train test valid
 def split_dataset(one_hot, labels, valid_frac=0.1, test_frac=0.2):
-        """split dataset into training, cross-validation, and test set"""
+	"""split dataset into training, cross-validation, and test set"""
 
-    def split_index(num_data, valid_frac, test_frac):
-        # split training, cross-validation, and test sets
+	def split_index(num_data, valid_frac, test_frac):
+		# split training, cross-validation, and test sets
 
-        train_frac = 1 - valid_frac - test_frac
-        cum_index = np.array(np.cumsum([0, train_frac, valid_frac, test_frac])*num_data).astype(int)
-        shuffle = np.random.permutation(num_data)
-        train_index = shuffle[cum_index[0]:cum_index[1]]
-        valid_index = shuffle[cum_index[1]:cum_index[2]]
-        test_index = shuffle[cum_index[2]:cum_index[3]]
+		train_frac = 1 - valid_frac - test_frac
+		cum_index = np.array(np.cumsum([0, train_frac, valid_frac, test_frac])*num_data).astype(int)
+		shuffle = np.random.permutation(num_data)
+		train_index = shuffle[cum_index[0]:cum_index[1]]
+		valid_index = shuffle[cum_index[1]:cum_index[2]]
+		test_index = shuffle[cum_index[2]:cum_index[3]]
 
-        return train_index, valid_index, test_index
+		return train_index, valid_index, test_index
 
 
-    # split training, cross-validation, and test sets
-    num_data = len(one_hot)
-    train_index, valid_index, test_index = split_index(num_data, valid_frac, test_frac)
+	# split training, cross-validation, and test sets
+	num_data = len(one_hot)
+	train_index, valid_index, test_index = split_index(num_data, valid_frac, test_frac)
 
-    # split dataset
-    train = (one_hot[train_index], labels[train_index,:])
-    valid = (one_hot[valid_index], labels[valid_index,:])
-    test = (one_hot[test_index], labels[test_index,:])
-    indices = [train_index, valid_index, test_index]
+	# split dataset
+	train = (one_hot[train_index], labels[train_index,:])
+	valid = (one_hot[valid_index], labels[valid_index,:])
+	test = (one_hot[test_index], labels[test_index,:])
+	indices = [train_index, valid_index, test_index]
 
-    return train, valid, test, indices
+	return train, valid, test, indices
 
 # TODO: random split into k-fold cross validation
 
