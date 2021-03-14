da36e08f61d9506bd1815a6ee643019d8366a380
koo-lab <koolaboratory.gmail.com>
2021-03-13 21:32:53 -0500

    added models in model_zoo to init

--- cipher/model_zoo/__init__.py
+++ cipher/model_zoo/__init__.py
@@ -1,1 +1,6 @@
 from . import deepbind  # noqa: F401
+from . import cnn_dist  # noqa: F401
+from . import cnn_local  # noqa: F401
+from . import deepbind_custom  # noqa: F401
+from . import residualbind  # noqa: F401
+
--- cipher/utils.py
+++ cipher/utils.py
@@ -29,3 +29,28 @@ def make_directory(dirpath, verbose=1):
     if not os.path.isdir(dirpath):
         os.mkdir(dirpath)
         print("making directory: " + dirpath)
+
+
+def import_model(model_name):
+    """Import a model from model_zoo. 
+
+    Parameters
+    ----------
+    model_name : string
+        Name of model in model_zoo to import.
+
+    Returns
+    -------
+        imported model 
+
+    Examples
+    --------
+    >>> model_name = 'deepbind'
+    >>> model = import_model(model_name)
+
+    """
+
+    # Import model from the zoo as singular animal
+    # Equivalent of `from model_zoo import model_name as animal` where model_name is evaluated at runtime
+    animal = __import__("cipher.model_zoo." + model_name, globals(), locals(), [model_name], 0) 
+    return animal
